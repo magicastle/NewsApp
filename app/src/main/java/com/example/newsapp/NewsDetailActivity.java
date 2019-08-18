5212e4157365edebc1360083cf9c15f106e74933
whhh <1372925742@qq.com>
2019-08-18 14:43:20 +0800

    back menu

--- app/src/main/java/com/example/newsapp/NewsDetailActivity.java
+++ app/src/main/java/com/example/newsapp/NewsDetailActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.newsapp.model.SingleNews;
 import com.google.android.material.appbar.AppBarLayout;
@@ -22,13 +23,15 @@ public class NewsDetailActivity extends AppCompatActivity {
     private TextView contentTextView;
     private TextView titleTextView;
     private SingleNews newsDetail;
+    //private Toolbar toolbar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
-
-
+//        toolbar = findViewById(R.id.toolb);
+//        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // find component
         newsDetail = (SingleNews) getIntent().getSerializableExtra("newsDetail");
         titleTextView = findViewById(R.id.new_detail_title_tv);
@@ -36,4 +39,30 @@ public class NewsDetailActivity extends AppCompatActivity {
         contentTextView= findViewById(R.id.new_detail_content_tv);
         contentTextView.setText(newsDetail.getContent());
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.hide,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.share:
+                Toast.makeText(this,"share",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mark:
+                Toast.makeText(this,"mark",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.download:
+                Toast.makeText(this,"download",Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+        }
+        return true;
+    }
+
 }
