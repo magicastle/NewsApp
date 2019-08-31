095b3be8a33a509bff7e1b34c610e650bd6bd65d
qistout <2817253603@qq.com>
2019-08-31 15:43:44 +0800

    添加标记项，使得不会频繁的进行新加载； 添加图片列表中图片显示

--- app/src/main/java/com/example/newsapp/adapter/MyNewsListAdapter.java
+++ app/src/main/java/com/example/newsapp/adapter/MyNewsListAdapter.java
@@ -2,14 +2,26 @@ package com.example.newsapp.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.example.newsapp.MainActivity;
 import com.example.newsapp.NewsDetailActivity;
 import com.example.newsapp.R;
@@ -36,6 +48,8 @@ public class MyNewsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         TextView title;
         TextView contentAbstract;
         TextView publishInfo;
+        ImageView image_right;
+        boolean isAdded;
 
         public MyViewHolder(View itemView){
             super(itemView);
@@ -43,6 +57,8 @@ public class MyNewsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             title = itemView.findViewById(R.id.item_newsTitle);
             contentAbstract = itemView.findViewById(R.id.item_newsAbstract);
             publishInfo = itemView.findViewById(R.id.item_newsPublishInfo);
+            image_right = itemView.findViewById(R.id.image_right);
+            isAdded = false;
 
             itemView.setOnClickListener(new View.OnClickListener(){
                 @Override
@@ -86,17 +102,59 @@ public class MyNewsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holders, int position) {
-        final MyViewHolder holder = (MyViewHolder) holders;
-        SingleNews news = newsList.get(position);
-        holder.title.setText(news.getTitle());
-        holder.contentAbstract.setText(news.getAbstract());
-        holder.publishInfo.setText("  " + news.getPublisher() + "     " + news.getPublishTime());
+        MyViewHolder holder = (MyViewHolder) holders;
+        if(holder.isAdded == false){
+                SingleNews news = newsList.get(position);
+                holder.title.setText(news.getTitle());
+                holder.contentAbstract.setText(news.getAbstract());
+                holder.publishInfo.setText("  " + news.getPublisher() + "     " + news.getPublishTime());
 
-        // TODO: 查看是否已经在History数据库中，如果有，就 Bina 灰 View
-        List<NewsCollectionsOrHistoryBean> list = historyDao.query(news.getNewsID());
-        if(list.size() == 0){
-            //
+                String urls[]= news.getImage();//images lists
+
+                if(urls.length>0)
+                {
+                    System.out.println(news.getTitle()+" "+urls[0]);
+
+                    RequestListener mRequestListener = new RequestListener() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target target, boolean isFirstResource) {
+                            Log.d("NewsDetailActivity", "onException: " + e.toString() + "  model:" + model + " isFirstResource: " + isFirstResource);
+                            holder.image_right.setImageResource(R.mipmap.ic_launcher);
+                            return false;
+                        }
+                        @Override
+                        public boolean onResourceReady(Object resource, Object model, Target target, DataSource dataSource, boolean isFirstResource) {
+                            Log.e("NewsDetailActivity",  "model:"+model+" isFirstResource: "+isFirstResource);
+                            return false;
+                        }
+                    };
+                    RequestOptions options = new RequestOptions()
+                            //.centerCrop()
+                            .placeholder(new ColorDrawable(Color.BLACK))
+                            .error(new ColorDrawable(Color.RED))
+                            .centerCrop()
+                            .priority(Priority.HIGH);
+
+                    Glide.with(context.getApplicationContext())
+                            .load( urls[0])
+                            .apply(options)
+                            .listener(mRequestListener)
+                            .into(holder.image_right);
+                    System.out.println("load....");
+                }
+                else{
+                    System.out.println(news.getTitle()+"  else......");
+                    holder.image_right.setVisibility(View.GONE);
+                }
+
+                holder.isAdded = true;
         }
+
+//        // TODO: 查看是否已经在History数据库中，如果有，就 Bina 灰 View
+//        List<NewsCollectionsOrHistoryBean> list = historyDao.query(news.getNewsID());
+//        if(list.size() == 0){
+//            //
+//        }
 
 
         // TODO: 点击效果
@@ -109,5 +167,11 @@ public class MyNewsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
     @Override
     public int getItemCount() {
         return newsList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        //return super.getItemViewType(position);
+        return position;
     }
 }
